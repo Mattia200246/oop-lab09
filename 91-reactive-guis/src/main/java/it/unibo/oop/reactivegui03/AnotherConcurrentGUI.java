85e9b1762f7fcf47ded8023f4dac0f085ba8f692
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2022-11-21 11:55:50 +0100

    copy/paste code from previus excercis. Now start real excercise

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -20,12 +20,14 @@ public final class AnotherConcurrentGUI extends JFrame {
     JLabel label = new JLabel();
     JButton up = new JButton("Up");
     JButton down = new JButton("Down");
+    JButton stop = new JButton("Stop");
     public AnotherConcurrentGUI(){
         final JPanel panel = new JPanel();
         panel.add(label);
         label.setText("0");
         panel.add(up);
         panel.add(down);
+        panel.add(stop);
 
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int)dimension.getWidth();
@@ -34,5 +36,53 @@ public final class AnotherConcurrentGUI extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.getContentPane().add(panel);
         this.setVisible(true);
+
+        Agent agent = new Agent();
+        new Thread(agent).start();
+
+        stop.addActionListener( (e)-> {agent.stop(); up.setEnabled(false); down.setEnabled(false);});
+        down.addActionListener( (e)-> agent.down());
+        up.addActionListener( (e)-> agent.up());
+
+        
     }
+
+    private class Agent implements Runnable{
+        private int counter = 0;
+        private volatile boolean option; //0 represent inc ; 1 dec
+        private volatile boolean stop;
+
+        @Override
+        public void run() {
+            while(!stop){
+                try {
+                    label.setText(Integer.toString(this.counter));
+                        if(!option){
+                            counter++;
+                        }else{
+                            counter--;
+                        }
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            
+        }
+
+        public void up() {
+            this.option = false;
+        }
+        private void stop() {
+            this.stop = true;
+            
+        }
+
+        private void down() {
+            this.option = true;
+        }
+        
+    }
+
 }
